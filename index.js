1147ed01cff7d4ce6f185b3f6746457c161fc726
leviathon <leviathon@Levs-Mac.local>
2015-10-14 14:12:11 -0700

    testing nodemailer

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 var sendgrid_username = process.env.SENDGRID_USERNAME;
 var sendgrid_password = process.env.SENDGRID_PASSWORD;
 var sendgrid_to_email = process.env.SENDGRID_TO_EMAIL;
@@ -36,22 +37,23 @@ app.post("/sendContact",function(req, res){
     var fromEmail = req.body.c_email;
     var message = req.body.c_message;
     var name = req.body.c_name;
+    console.log(req.body);
 
-    var transporter = nodemailer.createTransport({
-        service: "Gmail",
-        debug: true,
+    var transporter = nodemailer.createTransport(smtpTransport({
+        service: "gmail",
         auth: {
             user: sendgrid_to_email,
             pass: sendgrid_password
         }
-    });
+    }));
 
     var mailOptions = {
         from: fromEmail,
         to: sendgrid_to_email,
         subject: 'New Email from leviross.com',
         text: message,
-        html: ""
+        html: "<h3>From: " + name + "</h3>" + "<h5>Email: " + fromEmail + "</h5>"
+        + "<p>" + message + "</p>" 
     }
 
     transporter.sendMail(mailOptions, function (err, info) {
